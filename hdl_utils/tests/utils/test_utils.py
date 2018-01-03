cf3663dc86ee22b9177c17700160b70e7f234618
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2018-01-02 21:02:24 -0600

    Adding tests for utils.py

---
+++ hdl_utils/tests/utils/test_utils.py
@@ -0,0 +1,37 @@
+
+import unittest
+import os
+
+from hdl_utils import utils
+
+sLocalPath = os.path.join('.', 'hdl_utils', 'tests', 'utils')
+
+
+class testUtilsFunctions(unittest.TestCase):
+
+    def test_is_vhdl_file(self):
+        self.assertTrue(utils.is_vhdl_file('.vhd'))
+        self.assertTrue(utils.is_vhdl_file('.vhdl'))
+        self.assertFalse(utils.is_vhdl_file('.vho'))
+        self.assertFalse(utils.is_vhdl_file('.txt'))
+
+    def test_get_subdirs(self):
+        self.assertEqual(utils.get_subdirs(sLocalPath), [os.path.join(sLocalPath, 'subdir1')])
+        
+        lExpected = []
+        lExpected.append(os.path.join(sLocalPath, 'subdir1', 'subdir3'))
+        lExpected.append(os.path.join(sLocalPath, 'subdir1', 'subdir2'))
+        self.assertEqual(utils.get_subdirs(os.path.join(sLocalPath, 'subdir1')), lExpected)
+
+        lExpected = []
+        lExpected.append(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.1'))
+        lExpected.append(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.2'))
+        self.assertEqual(utils.get_subdirs(os.path.join(sLocalPath, 'subdir1', 'subdir2')), lExpected)
+
+    def test_subdir_has_init_file(self):
+        self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1')))
+        self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir2')))
+        self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.1')))
+        self.assertFalse(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.2')))
+        self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir3')))
+        self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir3', 'subdir3.1')))
