9ece73c4f810f775ddb92ec55929869c9708a00e
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2018-01-07 11:02:25 -0600

    Adding tests for existing functions.

--- hdl_utils/tests/utils/test_utils.py
+++ hdl_utils/tests/utils/test_utils.py
@@ -35,3 +35,19 @@ class testUtilsFunctions(unittest.TestCase):
         self.assertFalse(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.2')))
         self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir3')))
         self.assertTrue(utils.subdir_has_init_file(os.path.join(sLocalPath, 'subdir1', 'subdir3', 'subdir3.1')))
+
+    def test_subdir_has_vhdl_file(self):
+        self.assertFalse(utils.subdir_has_vhdl_file(os.path.join(sLocalPath, 'subdir1')))
+        self.assertFalse(utils.subdir_has_vhdl_file(os.path.join(sLocalPath, 'subdir1', 'subdir2')))
+        self.assertTrue(utils.subdir_has_vhdl_file(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.1')))
+        self.assertTrue(utils.subdir_has_vhdl_file(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.2')))
+        self.assertFalse(utils.subdir_has_vhdl_file(os.path.join(sLocalPath, 'subdir1', 'subdir3')))
+        self.assertTrue(utils.subdir_has_vhdl_file(os.path.join(sLocalPath, 'subdir1', 'subdir3', 'subdir3.1')))
+
+    def test_get_vhdl_files(self):
+        self.assertEqual(utils.get_vhdl_files(os.path.join(sLocalPath, 'subdir1')), [])
+        self.assertEqual(utils.get_vhdl_files(os.path.join(sLocalPath, 'subdir1', 'subdir2')), [])
+        self.assertEqual(utils.get_vhdl_files(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.1')), ['test.vhd'])
+        self.assertEqual(utils.get_vhdl_files(os.path.join(sLocalPath, 'subdir1', 'subdir2', 'subdir2.2')), ['test.vhd'])
+        self.assertEqual(utils.get_vhdl_files(os.path.join(sLocalPath, 'subdir1', 'subdir3')), [])
+        self.assertEqual(utils.get_vhdl_files(os.path.join(sLocalPath, 'subdir1', 'subdir3', 'subdir3.1')), ['test.vhd'])
