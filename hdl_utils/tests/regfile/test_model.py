4ed7ada6974a8290250bd43d5e4e435a8c017cee
Jeremiah Leary <jeremiah.c.leary@gmail.com>
2022-06-26 12:21:02 -0500

    Splitting tests between model and VHDL generator.

--- hdl_utils/tests/regfile/test_model.py
+++ hdl_utils/tests/regfile/test_model.py
@@ -4,25 +4,20 @@ import unittest
 
 from hdl_utils.regfile.model import memory_map
 
-from hdl_utils.tests.regfile import utils
-
-from hdl_utils.regfile.generator.vhdl import entity_generator
-
 sTestDir = os.path.dirname(__file__)
 
 sTestFile = os.path.join(sTestDir, 'atxmega_spi.json')
 
 oModel = memory_map.New(sTestFile)
 
-lExpected  = utils.read_file(os.path.join(sTestDir, 'entity.expected.vhd'))
-
-
 class test_entity(unittest.TestCase):
 
 
     def test_entity_generation(self):
         
-        lActual = entity_generator.generate(oModel)
-        self.assertEqual(lExpected, lActual)
+        self.assertTrue(oModel)
+        self.assertEqual('atxmega_spi', oModel.get_component_name())
+        self.assertEqual(0, oModel.get_address_offset())
+        self.assertEqual(4, len(oModel.get_children()))
 
 
